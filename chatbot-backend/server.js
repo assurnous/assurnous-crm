9d2a7735c7ff86e771b1e1683210a9cc9b45ab2f
Globalgreen2025 <globalgreen41@gmail.com>
2025-07-08 21:29:55 +0100

    update

--- chatbot-backend/server.js
+++ chatbot-backend/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 // middleware to connect with frontend
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://crm-assurnaux.vercel.app"],
     credentials: true,
 }));
 
